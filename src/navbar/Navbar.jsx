4cee603f46958f2eaa14137f09d09f7dc05f4f21
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-07 18:28:54 -0500

    Added a Boolean check.

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { NavWrapper, ListItem, StyledLink, Title, List } from './navbarStyles';
+import LoggedIn from '../navbar/LoginStatus/LoggedIn';
+import LoggedOut from '../navbar/LoginStatus/LoggedOut';
 
-const Navbar = () => {
+const Navbar = props => {
   return (
     <NavWrapper>
       <List>
         <ListItem>
           <Title>
-            <StyledLink href='#'>Test</StyledLink>
+            <StyledLink href='#'>My App</StyledLink>
           </Title>
         </ListItem>
+        {props.isLoggedIn ? <LoggedIn /> : <LoggedOut />}
       </List>
     </NavWrapper>
   );

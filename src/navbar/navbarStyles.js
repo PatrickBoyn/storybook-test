d0cb29187dde20c2a9247f44c81330d6df1cb681
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-07 18:00:30 -0500

    Added the basic styles.

---
+++ src/navbar/navbarStyles.js
@@ -0,0 +1,19 @@
+import styles from 'styled-components';
+
+export const NavWrapper = styles.nav`
+    display: flex;
+    background-color: #242424;
+`;
+
+export const Title = styles.h2``;
+
+export const List = styles.ul``;
+
+export const ListItem = styles.li`
+    list-style: none;
+`;
+
+export const StyledLink = styles.a`
+    text-decoration: none;
+    color: white;
+`;
